4176766b8c87de7378539b5bfd905ac0b6557731
Adam Kipnis <adamkipnis@yahoo.com>
2018-03-12 09:40:19 -0700

    Test deployments

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,15 @@
-var HelloWorld = artifacts.require("./HelloWorld.sol");
-var Reservations = artifacts.require("./HotelReservation");
+//var HelloWorld = artifacts.require("./HelloWorld.sol");
+//var MinTest = artifacts.require("./MinTest.sol");
+//var LoopExample = artifacts.require("./LoopExample.sol");
+var SimpleContract = artifacts.require("./SimpleContract.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(HelloWorld);
-  deployer.deploy(Reservations);
+//  deployer.deploy(HelloWorld).then(() => {
+//    return deployer.deploy(MinTest, HelloWorld.address);
+//  });
+
+//  deployer.deploy(MinTest);
+//  deployer.deploy(LoopExample);
+  deployer.deploy(SimpleContract);
+//  deployer.deploy(Reservations);
 };
